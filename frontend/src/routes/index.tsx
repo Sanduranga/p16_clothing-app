0a04b6cac95921bfde3388aedb51e6fdf218191a
Sanduranga <edirisinghest527@gmail.com>
2024-06-29 21:06:17 +0530

    remove clicked page contents

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,7 +2,6 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { HomePage } from "../pages/home";
 import { AddItemsPage } from "../pages/add-item-section";
-import ClickedItemPage from "../pages/clicked-item";
 import { SetDiscountPage } from "../pages/set-discount";
 import { MyStorePage } from "../pages/my-store";
 import { SaleItemsPage } from "../pages/sale-section";
@@ -23,10 +22,6 @@ export const router = [
       {
         path: "/add-items",
         element: <AddItemsPage />,
-      },
-      {
-        path: "/clicked-item/:code",
-        element: <ClickedItemPage />,
       },
       {
         path: "/set-discount/:code",
